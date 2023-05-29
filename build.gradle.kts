c71aaf46c3abdea127f4f4ef439c6e75c58454dd
Brian Norman <brian@bnorm.com>
2023-05-29 15:14:07 -0500

    Prepare for release 0.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.bnorm.robocode"
-version = "0.2.0-SNAPSHOT"
+version = "0.2.0"
 
 repositories {
     mavenCentral()
@@ -31,6 +31,7 @@ gradlePlugin {
             displayName = "Robocode Gradle Plugin"
             description = "Gradle plugin for developing Robocode bots"
             implementationClass = "com.bnorm.robocode.RobocodePlugin"
+            tags.set(listOf("robocode"))
         }
     }
 }
